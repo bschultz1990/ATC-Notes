8db59cb2fe54aebadf27155f85df252c3ecc8685
= <=>
2024-03-09 17:29:14 -0600

    Begin the refactorinating!

---
+++ radio.js
@@ -0,0 +1,40 @@
+let dep_loc = document.querySelectorAll('.dep-loc');
+let callsign = document.querySelectorAll('.callsign');
+let ac_type = document.querySelectorAll('.ac-type');
+let dep_gate = document.querySelectorAll('.dep-gate');
+let dep_atis = document.querySelectorAll('.dep-atis');
+let arr_loc = document.querySelectorAll('.arr-loc');
+let arr_rwy = document.querySelectorAll('.arr-rwy');
+let rwy_dst = document.querySelectorAll('.rwy-dst');
+
+let form_dep_loc = document.querySelector("#form-dep-loc");
+let form_callsign = document.querySelector("#form-callsign");
+let form_ac_type = document.querySelector("#form-ac-type");
+let form_dep_gate = document.querySelector("#form-dep-gate");
+let form_dep_atis = document.querySelector("#form-dep-atis");
+let form_arr_loc = document.querySelector("#form-arr-loc");
+let form_arr_runway = document.querySelector("#form-arr-rwy");
+let form_arr_dist = document.querySelector("#form-arr-dst");
+
+let update_btn = document.querySelector("#update");
+
+function helper(src, dest) {
+  for (let i = 0; i < dest.length; i++) {
+    if (src.value != "") {
+      dest[i].value = src.value;
+    }
+    // SOMETHING.style.color = 'red'
+  }
+}
+
+update_btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  helper(form_dep_loc, dep_loc)
+  helper(form_callsign, callsign)
+  helper(form_ac_type, ac_type)
+  helper(form_dep_gate, dep_gate)
+  helper(form_dep_atis, dep_atis)
+  helper(form_arr_loc, arr_loc)
+  helper(form_arr_runway, arr_rwy)
+  helper(form_arr_dist, rwy_dst)
+})
